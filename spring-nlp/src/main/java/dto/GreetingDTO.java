9028cf76f8916103d1d860c400912e78976722b2
Brandon <b.mazey@nyu.edu>
2018-04-19 12:25:31 -0400

    added base packages for Spring REST and JSON validators ...

---
+++ spring-nlp/src/main/java/dto/GreetingDTO.java
@@ -0,0 +1,30 @@
+package dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class GreetingDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private final long id;
+    private final String content;
+
+    public GreetingDTO(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
---
+++ spring-nlp/src/main/java/validator/GreetingValidator.java
@@ -0,0 +1,25 @@
+package validator;
+
+import dto.GreetingDTO;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+public class GreetingValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return GreetingDTO.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        GreetingDTO greeting = (GreetingDTO) obj;
+        if (checkInputString(greeting.getContent())) {
+            errors.rejectValue("name", "name.empty");
+        }
+    }
+
+    private boolean checkInputString(String input) {
+        return (input == null || input.trim().length() == 0);
+    }
+}
