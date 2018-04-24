2d7710d282f38da4188a18dbd23d23722b71d1d9
Brandon <b.mazey@nyu.edu>
2018-04-24 11:48:45 -0400

    added Zoo class for main method

---
+++ interview-challenge/src/main/java/zoo/Zoo.java
@@ -0,0 +1,14 @@
+package zoo;
+
+public class Zoo {
+
+    /**
+     * This Zoo class contains the main method - maybe we should try putting our animals here?
+     * @param args
+     */
+
+    public static void main(String [] args) {
+        Cat panther = new Panther();
+        panther.purr();
+    }
+}
