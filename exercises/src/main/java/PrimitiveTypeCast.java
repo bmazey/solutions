cda833882dc33296ea17b112621538dc2015a17f
Brandon <b.mazey@nyu.edu>
2018-04-26 20:00:18 -0400

    got my swagger back!

---
+++ exercises/src/main/java/PrimitiveTypeCast.java
@@ -0,0 +1,10 @@
+public class PrimitiveTypeCast {
+    public static void main(String[] args) {
+        double d = 100.5;
+        int i = (int)d; //explicit casting required here!
+        double dd = i; //implicit casting
+        System.out.println("original double value: " + d);
+        System.out.println("integer value: " + i);
+        System.out.println("new double value: " + dd);
+    }
+}
