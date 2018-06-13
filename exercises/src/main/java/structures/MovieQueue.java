32df3bc0de1cff420c2a936407321fdd051dff89
Brandon <b.mazey@nyu.edu>
2018-06-13 11:24:00 -0400

    adding movie tests

--- exercises/src/main/java/structures/MovieQueue.java
+++ exercises/src/main/java/structures/MovieQueue.java
@@ -1,7 +1,21 @@
 package structures;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MovieQueue {
+
     public static void main (String[] args) {
 
+    }
+
+    public Queue<String> createMovieQueue() {
+        Queue<String> myMovieQueue = new LinkedList<String>();
+        myMovieQueue.add("Leonardo");
+        myMovieQueue.add("Donatello");
+        myMovieQueue.add("Raphael");
+        myMovieQueue.add("Michelangelo");
+
+        return myMovieQueue;
     }
 }
--- exercises/src/test/java/structures/MovieQueueTest.java
+++ exercises/src/test/java/structures/MovieQueueTest.java
@@ -1,4 +1,44 @@
 package structures;
 
+import org.junit.Test;
+
+import java.util.Queue;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class MovieQueueTest {
+
+    private MovieQueue myMovieQueue = new MovieQueue();
+
+    @Test
+    public void checkFirstInLine() {
+        Queue<String> myQueue = myMovieQueue.createMovieQueue();
+        assertThat(myQueue.peek(), equalToIgnoringCase("Leonardo"));
+    }
+
+    @Test
+    public void checkSecondInLine() {
+        Queue<String> myQueue = myMovieQueue.createMovieQueue();
+        myQueue.remove();
+        assertThat(myQueue.peek(), equalToIgnoringCase("Donatello"));
+    }
+
+    @Test
+    public void checkQueueOrder() {
+        Queue<String> myQueue = myMovieQueue.createMovieQueue();
+        myQueue.remove();
+        myQueue.remove();
+        assertThat(myQueue.peek(), equalToIgnoringCase("Raphael"));
+        myQueue.remove();
+        assertThat(myQueue.peek(), equalToIgnoringCase("Michelangelo"));
+
+    }
+
+    @Test
+    public void checkQueueSize() {
+        Queue<String> myQueue = myMovieQueue.createMovieQueue();
+        assertThat(myQueue.size(), equalTo(4));
+    }
 }
