49f241a25ba3c0e7fdc67873b0876e8863c3b61e
Brandon <b.mazey@nyu.edu>
2018-06-13 14:55:53 -0400

    ok maps done too

--- exercises/src/main/java/structures/TreasureMap.java
+++ exercises/src/main/java/structures/TreasureMap.java
@@ -1,7 +1,20 @@
 package structures;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TreasureMap {
     public static void main (String[] args) {
 
+    }
+
+    public Map<String, String> createTreasureMap() {
+        Map<String, String> treasureMap = new HashMap<String, String>();
+        treasureMap.put("beach", "sandy shores");
+        treasureMap.put("coast", "ocean reef");
+        treasureMap.put("volcano", "hot lava");
+        treasureMap.put("x", "marks the spot");
+
+        return treasureMap;
     }
 }
--- exercises/src/test/java/structures/TreasureMapTest.java
+++ exercises/src/test/java/structures/TreasureMapTest.java
@@ -1,4 +1,35 @@
 package structures;
 
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.core.IsEqual.equalTo;
+
 public class TreasureMapTest {
+
+    TreasureMap myMap = new TreasureMap();
+    Map<String, String> treasureMap = myMap.createTreasureMap();
+
+    @Test
+    public void checkSandyBeach() {
+        assertThat(treasureMap.get("beach"), equalToIgnoringCase("sandy shores"));
+    }
+
+    @Test
+    public void checkSeaShore() {
+        assertThat(treasureMap.get("coast"), equalToIgnoringCase("ocean reef"));
+    }
+
+    @Test
+    public void checkVolcano() {
+        assertThat(treasureMap.get("volcano"), equalToIgnoringCase("hot lava"));
+    }
+
+    @Test
+    public void checkXMarksTheSpot() {
+        assertThat(treasureMap.get("x"), equalToIgnoringCase("marks the spot"));
+    }
 }
