a02e9a9dfd58b0657e63b4ac5979d175bbba195f
Brandon <b.mazey@nyu.edu>
2018-06-14 19:27:52 -0400

    basics for maze game up now

---
+++ exercises/src/main/java/patterns/factory/PlayMazeGame.java
@@ -0,0 +1,11 @@
+package patterns.factory;
+
+public class PlayMazeGame {
+
+    public static void main(String[] args) {
+        MazeGame ordinaryMazeGame = new OrdinaryMazeGame();
+        MazeGame magicMazeGame = new MagicMazeGame();
+
+        //what next ... ?
+    }
+}
